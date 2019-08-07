1598b27b1b8c762ea11edc0bcec0295b090951aa
Kurapati <kurapatl@ES.AD.ADP.COM>
2019-08-07 06:23:48 +0530

    updated changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SearchComponent } from './search/search.component';
+import { SidenavbarComponent } from './sidenavbar/sidenavbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SearchComponent,
+    SidenavbarComponent
   ],
   imports: [
     BrowserModule,
